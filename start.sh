be640efbade0859bec25e03e8e6a76b7bc1bb0af
race-in <shyamalmurmu740@gmail.com>
2026-03-09 19:48:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,12 +7,10 @@ mkdir -p $CONFIG
 cp -n /core.conf $CONFIG/core.conf
 cp -n /ltconfig.conf $CONFIG/ltconfig.conf
 
-echo "Starting Deluge daemon..."
 deluged -d
 
 sleep 8
 
-echo "Starting WebUI..."
 deluge-web --fork --port 8113
 
 tail -f /dev/null
