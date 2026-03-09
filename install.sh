5196bc1ddd60416228d4c444d68af1cdfbc6e1a9
race-in <shyamalmurmu740@gmail.com>
2026-03-09 19:47:24 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+echo "Installing Deluge 1.3.15 Docker..."
+
+# install dependencies
 apt update
 apt install -y docker.io git
 
+systemctl enable docker
+systemctl start docker
+
+# clone project
 git clone https://github.com/race-in/deluge1.3.15-docker.git
 
 cd deluge1.3.15-docker
 
+# run deploy
 bash deploy.sh
